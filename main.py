0777247bab49e579cce5358ab005a1d9bd0cc51a
eomy272-pixel <eomy272@gmail.com>
2026-07-17 10:20:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 
-# 환경 변수에서 값 가져오기
 receiver = os.environ.get('RECEIVER_EMAIL')
+print(f"DEBUG: 환경 변수에서 가져온 값은 '{receiver}' 입니다.")
 
-print("메일 발송 작업을 시작합니다!")
-print(f"목표 주소: {receiver}")
+if not receiver:
+    print("오류: RECEIVER_EMAIL 환경 변수를 찾을 수 없습니다!")
+else:
+    print(f"메일 발송 작업을 시작합니다! 목표 주소: {receiver}")
