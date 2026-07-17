e8422812f783e7da01a3d3c5cb3a36880823a5a4
eomy272-pixel <eomy272@gmail.com>
2026-07-17 11:26:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import smtplib
 from email.mime.text import MIMEText
 import os
 
-# 1. GitHub Secrets에서 정보 가져오기
-receiver = os.environ.get('RECEIVER_EMAIL')
-sender_email = "um0241@naver.com"  # 여기에 본인의 네이버 이메일을 입력하세요
-sender_pw = os.environ.get('NAVER_CLIENT_SECRET')
+# 1. 정보 설정
+receiver = "um0241@naver.com"  # 받는 사람 메일 주소
+sender_email = "um0241@naver.com"  # 보내는 사람 메일 주소
+sender_pw = os.environ.get('NAVER_CLIENT_SECRET') # GitHub Secrets에서 가져옴
 
 # 2. 메일 내용 만들기
 msg = MIMEText("테스트 메일이 성공적으로 발송되었습니다!")
 msg['Subject'] = "GitHub Actions 메일 발송 테스트"
 msg['To'] = receiver
+msg['From'] = sender_email
 
 # 3. 네이버 SMTP를 이용한 메일 발송
 try:
