2d225e8ed38eca0c67b755d51a74e715467fbaf2
eomy272-pixel <eomy272@gmail.com>
2026-07-17 10:25:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 
 receiver = os.environ.get('RECEIVER_EMAIL')
-print(f"DEBUG: 환경 변수에서 가져온 값은 '{receiver}' 입니다.")
-
-if not receiver:
-    print("오류: RECEIVER_EMAIL 환경 변수를 찾을 수 없습니다!")
+print(f"DEBUG_START")
+if receiver:
+    print(f"이메일이 확인되었습니다: {receiver[:2]}****") # 앞 두 글자만 보이게 함
 else:
-    print(f"메일 발송 작업을 시작합니다! 목표 주소: {receiver}")
+    print("오류: RECEIVER_EMAIL이 비어있습니다!")
+print(f"DEBUG_END")
